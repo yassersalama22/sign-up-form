e5cadd59b14d7f972f08c39e24b5f48ca1f401b5
Yasser Salama <yasser.salama@gmail.com>
2023-09-07 15:28:07 +0300

    Add Invalid Input Styles

---
+++ scripts.js
@@ -0,0 +1,8 @@
+let confirmPasswordInput = document.getElementById("confirmPassword");
+
+confirmPasswordInput.addEventListener("blur",ConfirmPasswordValidation);
+
+function ConfirmPasswordValidation()
+{
+    console.log("Confirm password validation...");
+}
